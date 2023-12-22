358e245fca83d69aff3da4a187123a9763e944fd
liqing1986 <63433475+liqing1986@users.noreply.github.com>
2023-12-22 14:39:04 +0800

    Update key.sh
    add check

--- key.sh
+++ key.sh
@@ -14,6 +14,7 @@ Usage:
         key.sh [options...] <arg>
 
 Options:
+  -c    检查公钥认证开启状态
   -u    从github安装公钥
   -f    从本地文件安装公钥
   -d    禁止密码登录"
@@ -22,6 +23,23 @@ if [ $# -eq 0 ]; then
     USAGE
     exit 1
 fi
+check_key(){
+    $SUDO grep -q "^PubkeyAuthentication yes" /etc/ssh/sshd_config
+    if [ $? -eq 0 ]; then
+        echo "${INFO} 公钥验证已启用"
+    else
+        echo "${INFO} 公钥验证未启用"
+        read -p "是否要开启公钥验证？(y/n): " choice
+        if [ "$choice" == "y" ] || [ "$choice" == "Y" ]; then
+        # 启用公钥验证
+            $SUDO sed -i 's/^#PubkeyAuthentication.*/PubkeyAuthentication yes/' /etc/ssh/sshd_config
+            RESTART_SSHD=1
+            echo "${INFO} 公钥验证已启用"
+        else
+            echo "${INFO} 未进行任何更改"
+        fi
+    fi
+}
 install_key() {
     [ "${PUB_KEY}" == '' ] && echo "${ERROR} ssh key does not exist." && exit 1
     if [ ! -f "${HOME}/.ssh/authorized_keys" ]; then
@@ -81,6 +99,9 @@ disable_password() {
 }
 while getopts 'u:f:d:' OPT; do
     case $OPT in
+    c)
+        check_key()
+        ;;
     u)
         KEY_ID=$OPTARG
         get_github_key
